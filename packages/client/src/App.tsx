cabfac8d8e210d9d6db91324aa3878d85fd933f9
yoGhastly <ve06976@innovaccion.mx>
2022-09-10 20:52:55 -0500

    Both client and server connected

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { trpc } from './trpc'
+
+import "./index.scss";
+
+const client = new QueryClient();
+
+const AppContent = () => {
+  const hello = trpc.useQuery(['hello'])
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <h1 className="uppercase">{JSON.stringify(hello.data)}</h1>
+    </div>
+
+  )
+}
+
+const App = () => {
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      url: 'http://localhost:8080/trpc',
+    })
+  );
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={client}>
+      <QueryClientProvider client={client}>
+        <AppContent />
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
